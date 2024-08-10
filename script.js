c3ad57ec0c073ee0c8b24cd819a391540a3a77a8
Askela574 <josuefernando574@gmail.com>
2024-08-10 00:22:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+onload = () => {
+  document.body.classList.remove("container");
+
+  setTimeout(() => {
+    alert("Espero que te recuperes mi bonita, esto es para ti.💗");
+    window.location.href = "https://www.youtube.com/watch?v=Mcj75l2gJcY"; // Reemplaza con el enlace de tu elección
+  }, 10000); // 10000 milisegundos = 15 segundos
+};
