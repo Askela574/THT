b73c4848723f7f4fdda05608676600b23eafbb81
Askela574 <josuefernando574@gmail.com>
2025-01-20 08:11:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ onload = () => {
   document.body.classList.remove("container");
 
   setTimeout(() => {
-    alert("Espero que te recuperes mi bonita, esto es para ti.💗");
+    alert("Mi amor precioso se que lo vas a lograr eres un sol asi que animooo todo va a estár bien 💗");
     window.location.href = "https://www.youtube.com/watch?v=Mcj75l2gJcY"; // Reemplaza con el enlace de tu elección
   }, 10000); // 10000 milisegundos = 15 segundos
 };
