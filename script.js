ab3609c8f9e1f5cc979b1f7c3b80c444afe259fe
Askela574 <josuefernando574@gmail.com>
2025-01-20 08:17:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ onload = () => {
   document.body.classList.remove("container");
 
   setTimeout(() => {
-    alert("Mi amor precioso se que lo vas a lograr eres un sol asi que animooo todo va a estár bien 💗");
+    alert("Mi amor precioso se que lo vas a lograr eres un sol asi que animooo todo va a estár bien no olvides que te amo un mundo 💗");
     window.location.href = "https://www.youtube.com/watch?v=Mcj75l2gJcY"; // Reemplaza con el enlace de tu elección
   }, 10000); // 10000 milisegundos = 15 segundos
 };
